040e711024c82aba6c25f31c1ae335ac2ab89a1a
nebbilies <hirohamada17092005@gmail.com>
2025-10-21 16:31:44 +0700

    feat: enhance Header component

--- src/app/(storefront)/components/Header/Header.tsx
+++ src/app/(storefront)/components/Header/Header.tsx
@@ -1,12 +1,49 @@
+import Icon from '@/components/Icon';
+import Link from "next/link";
 import Image from "next/image";
 
+const navigationItems : Record<string, string> = {
+    HOME: '/',
+    ABOUT: '/about',
+    SHOP: '/shop',
+    ARTICLES: '/articles',
+    CONTACT: '/contact',
+};
 
 export default function Header() {
     return (
-        <header className={'min-w-screen flex flex-col items-start'}>
-            <Image src={'/logo.svg'} alt="Storefront Logo" width={121} height={32} />
-
-            <h1>Welcome to My Storefront</h1>
+        <header className={'min-w-screen w-screen flex flex-col'}>
+            <div className={'flex w-full px-60 justify-end py-2'}>
+                <div className={'flex gap-10 items-center font-inter text-[12px]'}>
+                    <div
+                        className={'flex w-full gap-1 items-center hover:text-gray-500 transition-colors duration-300 cursor-pointer'}>
+                        <Icon name={"person"} size={16}/>
+                        <span className={''}>Account</span>
+                    </div>
+                    <div className={'border-l h-4 border-[#e0e0e0]'}/>
+                    <div
+                        className={'flex w-full gap-1 items-center hover:text-gray-500 transition-colors duration-300 cursor-pointer'}>
+                        <Icon name={"cart"} size={16}/>
+                        <span className={''}>Cart</span>
+                    </div>
+                </div>
+            </div>
+            <div className={'w-full border-b border-[#e0e0e0]'}/>
+            <div className={'w-full flex justify-between px-60'}>
+                <Image src={'/Logo.svg'} alt={'Logo'} width={160} height={40} className={'py-4'}/>
+                <nav className={'flex gap-0 items-center'}>
+                    {Object.entries(navigationItems).map(([name, path]) => (
+                        <Link
+                            key={name}
+                            href={path}
+                            className={'px-6 h-full font-inter flex items-center text-sm font-medium hover:bg-gray-300 transition-colors duration-300'}
+                        >
+                            {name}
+                        </Link>
+                    ))}
+                </nav>
+            </div>
+            <div className={'w-full border-b border-[#e0e0e0]'}/>
         </header>
     );
 }
