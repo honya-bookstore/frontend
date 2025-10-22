de5bbe3611531b6c1e700a40aa6c4998b64bec8f
nebbilies <hirohamada17092005@gmail.com>
2025-10-22 13:01:45 +0700

    feat: update Footer component layout and styles; add Cormorant font

--- src/app/(storefront)/components/Footer/Footer.tsx
+++ src/app/(storefront)/components/Footer/Footer.tsx
@@ -1,8 +1,27 @@
+import {Span} from "next/dist/server/lib/trace/tracer";
+import Icon from "@/components/Icon";
 
 export default function Footer() {
     return (
-        <footer>
-            <p>© 2024 My Storefront. All rights reserved.</p>
+        <footer className={'w-screen h-auto flex flex-col bg-background-darker'}>
+            <div className={'w-full flex justify-between items-center px-100 py-10 '}>
+                <div className={'font-cormorant text-5xl'}>
+                    <span className={'font-bold'}>BOOK</span>
+                    <span className={'font-light'}>STORE</span>
+                </div>
+                <div className={'font-cormorant text-4xl flex flex-col items-center justify-center opacity-55'}>
+                    <span className={'font-bold'}>CONTACT <span className={'font-normal'}>US</span> </span>
+                    <div className={'flex items-center justify-between gap-6 mt-4'}>
+                        <Icon name={"facebook"} size={24} className={"cursor-pointer hover:opacity-70 transition-opacity duration-300"}/>
+                        <Icon name={"twitter"} size={24} className={"cursor-pointer hover:opacity-70 transition-opacity duration-300"}/>
+                        <Icon name={"instagram"} size={24} className={"cursor-pointer hover:opacity-70 transition-opacity duration-300"}/>
+                        <Icon name={"linkedin"} size={24} className={"cursor-pointer hover:opacity-70 transition-opacity duration-300"}/>
+                    </div>
+                </div>
+            </div>
+            <div className={'w-full text-center opacity-50 text-md font-inter py-1'}>
+                @ 2025 UIT. SE357 Course Project.
+            </div>
         </footer>
     );
 }
