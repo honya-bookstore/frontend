146c37a6506c6d57a6280068eb27d22c6a3ab015
nebbilies <hirohamada17092005@gmail.com>
2025-12-12 22:10:22 +0700

    feat: add fetcher utility function for client-side API requests

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,19 @@ export function getBookCover(media: BookMedia[]) {
     const cover = media.find((med) => med.isCover);
     return cover ? cover.url : "images/fallbackBookImage.png"
 }
+
+export const fetcher = (
+    url: string,
+    token: string,
+) =>
+    fetch(url, {
+        headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${token}`,
+        },
+    }).then((res) => {
+        if (!res.ok) {
+            throw new Error(`Failed to fetch ${url}`);
+        }
+        return res.json();
+    });
