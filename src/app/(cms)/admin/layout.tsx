12449328a1a7911756a906cfc02de5c6d35fcf44
nebbilies <hirohamada17092005@gmail.com>
2025-12-14 22:39:51 +0700

    feat: add metadata and Toaster for CMS layout

--- src/app/(cms)/admin/layout.tsx
+++ src/app/(cms)/admin/layout.tsx
@@ -4,6 +4,18 @@ import {UIProvider} from "@/app/(cms)/_context/UIContext";
 import CMSFooter from "@/app/(cms)/_components/Footer";
 import Breadcrumb from "@/app/(storefront)/_components/Breadcrumb/Breadcrumb";
 import {headers} from "next/headers";
+import {Toaster} from "sonner";
+import {ConfirmationProvider} from "@/app/(cms)/_context/AlertDialogContext";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | CMS - Honya Bookstore',
+        default: 'CMS - Honya Bookstore',
+    },
+    description: 'Content Management System for Honya Bookstore',
+};
+
 
 export default async function CMSLayout({
     children,
@@ -20,21 +32,24 @@ export default async function CMSLayout({
 
     return (
         <UIProvider>
-            <div className="flex h-screen w-screen overflow-hidden bg-page-background">
-                <NavigationBar/>
-                <div className={'flex flex-col justify-between w-full h-full'}>
-                    <CMSHeader/>
-                    <div className={'mt-4 ml-6'}>
-                        <Breadcrumb items={breadcrumbItems}/>
-                    </div>
-                    <main className="m-6 mt-4 flex-1 flex flex-col gap-4 overflow-y-auto">
-                        <div className={'overflow-y-scroll bg-white rounded-lg shadow-md p-6 min-h-full'}>
-                            {children}
+            <ConfirmationProvider>
+                <div className="flex h-screen w-screen overflow-hidden bg-page-background">
+                    <NavigationBar/>
+                    <div className={'flex flex-col justify-between w-full h-full'}>
+                        <Toaster position="bottom-right" richColors/>
+                        <CMSHeader/>
+                        <div className={'mt-4 ml-6'}>
+                            <Breadcrumb items={breadcrumbItems}/>
                         </div>
-                    </main>
-                    <CMSFooter/>
+                        <main className="m-6 mt-4 flex-1 flex flex-col gap-4 overflow-y-auto">
+                            <div className={'overflow-y-scroll bg-white rounded-lg shadow-md p-6 min-h-full'}>
+                                {children}
+                            </div>
+                        </main>
+                        <CMSFooter/>
+                    </div>
                 </div>
-            </div>
+            </ConfirmationProvider>
         </UIProvider>
     );
 }
--- src/app/(cms)/admin/page.tsx
+++ src/app/(cms)/admin/page.tsx
@@ -1,4 +1,10 @@
 import NavigationBar from "@/app/(cms)/_components/NavigationBar";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'Dashboard',
+    description: 'dashboard for managing the bookstore',
+}
 
 export default function DashboardPage() {
     return (
